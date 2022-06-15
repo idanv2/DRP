94dad9db533793fcac41b5ffdf5bd9675ade292b
idanversano <idan.versano87@gmail.com>
2022-06-15 15:44:38 +0300

    13_6

--- classes_version.py
+++ classes_version.py
@@ -18,11 +18,14 @@ class Custom_Dataset(torch.utils.data.dataset.Dataset):
     def __len__(self):
         return len(self.dataset)
 class par:
+    frog1=2
     frog=2;batch_size=10
     nx=20;ny=20;ymin, ymax = 0.0, 1.0;xmin, xmax = 0.0, 1.0;Z=1
     T=1;time_steps = 400; dt = T / time_steps
     lx = xmax - xmin;ly = ymax - ymin;dx = lx / (nx - 1);dy = ly / (ny - 1)
     w_yee = torch.tensor([1.],dtype=float)
+    bc_filters = torch.tensor([-1., 1.], dtype=float).reshape(1, 1, 2)
+
 
 class Network(par,nn.Module):
 
@@ -38,25 +41,39 @@ class Network(par,nn.Module):
          Hy=X3.clone()
          w1=self.params[0]
          filters = torch.cat(((w1 - 1) / 3, -w1, w1, (1 - w1) / 3), 0).reshape(1, 1, 4)
-         bc_filters = torch.tensor([-1., 1.], dtype=float).reshape(1, 1, 2)
          E0 = E.clone()
          E01 = E.clone()
          Hx0 = Hx.clone()
          Hy0 = Hy.clone()
          Hx01 = Hx.clone()
          Hy01 = Hy.clone()
-         E[1:par.nx - 1, 1:par.ny - 1] = amper(E0, Hx0, Hy0, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, bc_filters, 1)
-         # E[1:nx - 1, 1:ny - 1] = amper(E0, Hx0, Hy0, Z, dt, dx, dy, nx, ny, bc_filters, 1)
-         E[par.frog:par.nx - par.frog, par.frog:par.ny - par.frog] = amper(E01, Hx01, Hy01, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, filters, par.frog)
+         E[1:par.nx - 1, 1:par.ny - 1] = self.amper(E0, Hx0, Hy0,par.bc_filters,1)
+         E[par.frog:par.nx - par.frog, par.frog:par.ny - par.frog] = self.amper(E01, Hx01, Hy01,filters,par.frog)
          Em = E.clone()
          Em1 = E.clone()
-
-         Hx[1:par.nx - 1, 0:par.ny - 1] = faraday(Em, Hx0, Hy0, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, bc_filters, 1)[0]
-         Hy[0:par.nx - 1, 1:par.ny - 1] = faraday(Em, Hx0, Hy0, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, bc_filters, 1)[1]
-         Hx[par.frog:par.nx - par.frog, par.frog - 1:par.ny - par.frog] = faraday(Em1, Hx01, Hy01, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, filters, par.frog)[0]
-         Hy[par.frog - 1:par.nx - par.frog, par.frog:par.ny - par.frog] = faraday(Em1, Hx01, Hy01, par.Z, par.dt, par.dx, par.dy, par.nx, par.ny, filters, par.frog)[1]
-
+         Hx[1:par.nx - 1, 0:par.ny - 1] = self.faraday(Em, Hx0, Hy0,par.bc_filters,1)[0]
+         Hy[0:par.nx - 1, 1:par.ny - 1] = self.faraday(Em, Hx0, Hy0,par.bc_filters,1)[1]
+         Hx[par.frog:par.nx - par.frog, par.frog - 1:par.ny - par.frog] = self.faraday(Em1, Hx01, Hy01, filters,par.frog)[0]
+         Hy[par.frog - 1:par.nx - par.frog, par.frog:par.ny - par.frog] = self.faraday(Em1, Hx01, Hy01, filters,par.frog)[1]
          return [E, Hx, Hy]
+    def amper(self,E,Hx,Hy,filters,step):
+        S1 = (par.Z * par.dt / par.dx) * F.conv1d(torch.transpose(Hy, 0, 1).reshape(par.ny, 1, par.nx), filters).reshape(par.ny, par.nx - (
+                    2 * step - 1)).transpose(1, 0)[
+                             0:-1, step:par.ny - step]
+        S2 = (par.Z * par.dt / par.dy) * (F.conv1d(Hx.reshape(par.nx, 1, par.ny), filters).reshape(par.nx, par.ny - (2 * step - 1)))[
+                             step:par.nx - step, 0:-1]
+        return E[step:par.nx - step, step:par.ny - step] + S1 - S2
+    def faraday(self,E,Hx,Hy,filters,step):
+        S3 = (par.dt / (par.Z * par.dy)) * F.conv1d(E.reshape(par.nx, 1, par.ny), filters).reshape(par.nx, par.ny - (2 * step - 1))[step:par.nx - step,
+                               0:]
+        S4 = (par.dt / (par.Z * par.dx)) * F.conv1d(torch.transpose(E, 0, 1).reshape(par.ny, 1, par.nx), filters).reshape(par.ny, par.nx - (
+                    2 * step - 1)).transpose(1, 0)[0:,
+                               step:par.ny - step]
+
+        Ax = Hx[step:par.nx - step, step - 1:par.ny - step] - S3
+        Ay = Hy[step - 1:par.nx - step, step:par.ny - step] + S4
+        return [Ax, Ay]
+
     def loss(self,x,y):
         q=0
         for k in range(len(x)):
@@ -70,33 +87,30 @@ k1=1.
 k2=1.
 E_a,Hx_a,Hy_a=create_train(par.nx, par.ny, par.dx, par.dy, par.dt, par.time_steps,k1,k1)
 data=[]
-for k in range(par.time_steps):
-    x=torch.cat((E_a[k],Hx_a[k],Hy_a[k]),0)
-    y=torch.cat((E_a[k+1],Hx_a[k+1],Hy_a[k+1]),0)
-    data.append((x,y))
-train_loader = torch.utils.data.DataLoader(dataset=Custom_Dataset(data),
-                                           batch_size=par.batch_size,
-                                           shuffle=False)
-# training loop
-loss=0
-for i,data in enumerate(train_loader):
+E1=E_a[4]
+Hx1=Hx_a[4]
+Hy1=Hy_a[4]
+N.forward(E1,Hx1,Hy1)
 
-    for k in range(par.batch_size):
-        f=N.forward(data[0][k,0:par.nx,0:par.ny],data[0][k,par.nx:2*par.nx,0:par.nx],data[0][k,2*par.nx:3*par.nx,0:par.nx])
-        loss+=N.loss(f,[data[1][k,0:par.nx,0:par.ny],data[1][k,par.nx:2*par.nx,0:par.nx],data[1][k,2*par.nx:3*par.nx,0:par.nx]])
-        print(loss)
+# for k in range(par.time_steps):
+#     x=torch.cat((E_a[k],Hx_a[k],Hy_a[k]),0)
+#     y=torch.cat((E_a[k+1],Hx_a[k+1],Hy_a[k+1]),0)
+#     data.append((x,y))
+# train_loader = torch.utils.data.DataLoader(dataset=Custom_Dataset(data),
+#                                            batch_size=par.batch_size,
+#                                            shuffle=False)
+# # training loop
+# loss=0
+# for i,data in enumerate(train_loader):
+#
+#     for k in range(par.batch_size):
+#         f=N.forward(data[0][k,0:par.nx,0:par.ny],data[0][k,par.nx:2*par.nx,0:par.nx],data[0][k,2*par.nx:3*par.nx,0:par.nx])
+#         loss+=N.loss(f,[data[1][k,0:par.nx,0:par.ny],data[1][k,par.nx:2*par.nx,0:par.nx],data[1][k,2*par.nx:3*par.nx,0:par.nx]])
+#         print(loss)
+#
+#
+#
 
-
-
-
-#print(v[-1,:,:]-x)
-
-
-#N=Network(par.w_yee)
-#print(type(Train[0]))
-#print(Train[0])
-#print(N.params)
-#N.forward(E_a[1],Hx_a[1],Hy_a[1])
 
 
 
